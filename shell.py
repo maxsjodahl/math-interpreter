8fe8fb2cd2a5b76df6c05e83bbe3b56a5038d529
Max <max.sjodahl12@gmail.com>
2025-03-17 22:59:05 +0100

    fixed error printing

--- shell.py
+++ shell.py
@@ -1,17 +1,20 @@
+import sys
 from lexer import Lexer
 from parser import Parser
 
 
 while True:
-    text = input("> ")
-    # text = "2+3*5"
-    lex = Lexer(text)
-    pars = Parser(text)
     try:
+        text = input("> ")
+        # text = "2+3*5"
+        lex = Lexer(text)
+        pars = Parser(text)
+    
         tokens = lex.tokenize()
         print(f"{tokens}")
         ast = pars.parse()
         print(f'{ast}')
+
     except Exception as e:
-        print(f"{e}")
-        break
+        # print the error without the traceback
+        print(e, file=sys.stderr)
