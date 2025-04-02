6f3449ce189bbc80a5b3a6d79dbfb6900a8cdd2d
Max <max.sjodahl12@gmail.com>
2025-04-03 00:40:45 +0200

    refactor: added main guard

--- main.py
+++ main.py
@@ -12,68 +12,73 @@ def extention_validation(filepath):
     return filepath
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-o", "--output", help="specified output file (optional)")
-parser.add_argument(
-    "-s",
-    "--show",
-    action="store_true",
-    help=" shows the generated tokens and AST (optional)",
-)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--output", help="specified output file (optional)")
+    parser.add_argument(
+        "-s",
+        "--show",
+        action="store_true",
+        help=" shows the generated tokens and AST (optional)",
+    )
 
-group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument(
-    "-cl", "--cline", help="uses command-line argument as input expressions"
-)
-group.add_argument(
-    "-f", "--filepath", type=extention_validation, help="uses file as input (*.mat)"
-)
-args = parser.parse_args()
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument(
+        "-cl", "--cline", help="uses command-line argument as input expressions"
+    )
+    group.add_argument(
+        "-f", "--filepath", type=extention_validation, help="uses file as input (*.mat)"
+    )
+    args = parser.parse_args()
 
-try:
-    if args.cline:
-        text = args.cline
-    elif args.filepath:
-        with open(args.filepath, "r") as file:
-            text = file.read()
-            print(text)
+    try:
+        if args.cline:
+            text = args.cline
+        elif args.filepath:
+            with open(args.filepath, "r") as file:
+                text = file.read()
+                print(text)
 
-    errorText = ErrorText(text)
-    lex = Lexer(text)
-    tokens = lex.tokenize()
+        errorText = ErrorText(text)
+        lex = Lexer(text)
+        tokens = lex.tokenize()
 
-    pars = Parser(tokens)
-    ast = pars.parse()
+        pars = Parser(tokens)
+        ast = pars.parse()
 
-    if args.show:
-        print(f"-------------\nTokens:\n{tokens}")
-        print(f"-------------\nAbstract Syntax Tree:\n{ast}\n")
+        if args.show:
+            print(f"-------------\nTokens:\n{tokens}")
+            print(f"-------------\nAbstract Syntax Tree:\n{ast}\n")
 
-    inter = Interpreter(ast)
-    result = inter.run()
+        inter = Interpreter(ast)
+        result = inter.run()
 
-    ptext = re.split("\n|;", text)
+        ptext = re.split("\n|;", text)
 
-    s = []
-    for i, line in enumerate(ptext):
-        st = line.strip()
-        if st == "":
+        s = []
+        for i, line in enumerate(ptext):
+            st = line.strip()
+            if st == "":
+                s.append("\n")
+                continue
+
+            s.append(f"{st} = {result[i]}")
             s.append("\n")
-            continue
 
-        s.append(f"{st} = {result[i]}")
-        s.append("\n")
+        if args.output:
+            f = open(args.output, "x")
+            f.write("".join(s[:-1]))
+        else:
+            print("Output:")
+            print("".join(s[:-1]))
 
-    if args.output:
-        f = open(args.output, "x")
-        f.write("".join(s[:-1]))
-    else:
-        print("Output:")
-        print("".join(s[:-1]))
+    except CustomError as e:
+        print(e)
+        print(errorText.makeErrorText(e.pos))
 
-except CustomError as e:
-    print(e)
-    print(errorText.makeErrorText(e.pos))
+    except Exception as e:
+        print(e)
 
-except Exception as e:
-    print(e)
+
+if __name__ == "__main__":
+    main()
